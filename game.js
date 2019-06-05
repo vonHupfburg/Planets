23301667e6ae0ebfc2358c93671893f26f3e339c
vonHupfburg <csabasteiber@gmail.com>
2019-06-05 22:01:50 +0200

    feat: orbits can be defined around any object
    Orbits can now be defined around any object, not just the Sun. This allows the creation of moons and such.

--- game.js
+++ game.js
@@ -3,15 +3,8 @@ function draw() {
   ctx.clearRect(0, 0, 1000, 1000);
 
   // Sun
-  ctx.beginPath();
-  ctx.arc(sunLocX, sunLocY, 100, 0, 2*Math.PI);
-  ctx.fillStyle = "yellow";
-  ctx.fill();
-
-
   for (var index = 0; index < orbits.length; index++){
-    orbits[index].drawOrbit(ctx);
-    orbits[index].drawPlanet(ctx);
+    orbits[index].drawAll(ctx);
   }
 
   window.requestAnimationFrame(draw);
@@ -19,47 +12,73 @@ function draw() {
 }
 
 class Orbit {
-  constructor(distanceFromSun, orbitalPeriod, fillStyle, planetWidth) {
-    this.distanceFromSun = distanceFromSun;
-    this.orbitalPeriod = orbitalPeriod;
+  constructor(orbitThis, orbitDistance, orbitPeriod, fillStyle, planetWidth) {
+    this.orbitThis = orbitThis;
+    this.orbitDistance = orbitDistance;
+    this.orbitPeriod = orbitPeriod;
+    this.orbitCurrent = 0;
+    this.locX = this.getNextLocX();
+    this.locY = this.getNextLocY();
     this.fillStyle = fillStyle;
     this.planetWidth = planetWidth;
   }
 
+  getNextLocs(){
+    var orbitalTime = time - (Math.floor(time/this.orbitPeriod) * this.orbitPeriod);
+    this.orbitCurrent = (orbitalTime / this.orbitPeriod);
+    this.locX = this.getNextLocX();
+    this.locY = this.getNextLocY();
+  }
+
+  getNextLocX(){
+    if (this.orbitThis === null){
+      return 500;
+    } else {
+      return this.orbitThis.locX + this.orbitDistance * Math.sin(2*Math.PI*this.orbitCurrent);
+    }
+  }
+
+  getNextLocY(){
+    if (this.orbitThis === null){
+      return 500;
+    } else {
+      return this.orbitThis.locY + this.orbitDistance * Math.cos(2*Math.PI*this.orbitCurrent);
+    }
+  }
+
+  drawAll(ctx){
+    if (this.orbitThis !== null){
+      this.drawOrbit(ctx);
+      this.getNextLocs();
+    }
+    this.drawPlanet(ctx);
+  }
+
   drawOrbit(ctx){
-    ctx.save();
     ctx.beginPath();
-    ctx.arc(sunLocX, sunLocY, this.distanceFromSun, 0, 2*Math.PI)
+    ctx.arc(this.orbitThis.locX, this.orbitThis.locY, this.orbitDistance, 0, 2*Math.PI)
     ctx.stroke();
-    ctx.restore();
   }
 
   drawPlanet(ctx){
-    var orbitalTime = time - (Math.floor(time/this.orbitalPeriod) * this.orbitalPeriod);
-    ctx.save();
-    ctx.translate(sunLocX, sunLocY);
-    ctx.rotate(Math.PI * 2 * (orbitalTime/this.orbitalPeriod));
-    ctx.translate(-sunLocX, -sunLocY);
     ctx.beginPath();
-    ctx.arc(sunLocX, sunLocY + this.distanceFromSun, this.planetWidth, 0, 2*Math.PI);
+    ctx.arc(this.locX, this.locY, this.planetWidth, 0, 2*Math.PI);
     ctx.fillStyle = this.fillStyle;
     ctx.fill();
-    ctx.restore();
   }
 }
 
-var sunLocX = 500;
-var sunLocY = 500;
 var orbits = [];
 var time = 0;
 
-orbits.push(new Orbit(120, 100, "#ff9000", 5));
-orbits.push(new Orbit(170, 225, "#aaaaaa", 15));
-orbits.push(new Orbit(220, 365, "#30aaaa", 20));
-orbits.push(new Orbit(270, 687, "#909000", 12));
-orbits.push(new Orbit(320, 12*365, "orange", 30));
-orbits.push(new Orbit(370, 20*365, "yellow", 22));
-orbits.push(new Orbit(420, 84*365, "#00ffff", 20));
-orbits.push(new Orbit(470, 165*365, "#0000ff", 25));
-
+orbits.push(new Orbit(null, 0, 0, '#ffff00', 50));
+orbits.push(new Orbit(orbits[0], 120, 100, "#ff9000", 5));
+orbits.push(new Orbit(orbits[0], 170, 225, "#aaaaaa", 15));
+orbits.push(new Orbit(orbits[0], 220, 365, "#30aaaa", 20));
+orbits.push(new Orbit(orbits[0], 270, 687, "#909000", 12));
+orbits.push(new Orbit(orbits[0], 320, 12*365, "orange", 30));
+orbits.push(new Orbit(orbits[0], 370, 20*365, "yellow", 22));
+orbits.push(new Orbit(orbits[0], 420, 84*365, "#00ffff", 20));
+orbits.push(new Orbit(orbits[0], 470, 165*365, "#0000ff", 25));
+orbits.push(new Orbit(orbits[3], 30, 27, '#909090', 5));
 window.requestAnimationFrame(draw);
